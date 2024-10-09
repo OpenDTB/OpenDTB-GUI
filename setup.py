7d7cfafeef6377c417d482e35142775f54b2904b
yihang.wang@nlpr.ia.ac.cn <yihang.wang@nlpr.ia.ac.cn>
2024-10-09 15:59:52 +0800

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="OpenDTB-gui",
-    version="0.2.6",
+    version="0.0.1",
     description="CASIA's digital twin brain platform",
     author="Brain Netom Group",
     url="https://github.com/OpenDTB/OpenDTB-GUI",
     license="GPL-3.0",
     install_requires=[
-        "OpenDTB-main @ https://github.com/OpenDTB/OpenDTB-MAIN/archive/refs/tags/v0.2.6.tar.gz",
+        "OpenDTB-main @ https://github.com/OpenDTB/OpenDTB-MAIN/archive/refs/tags/v0.0.1.tar.gz",
         "PyQt-Fluent-Widgets",
         "lmdb",
         "qtconsole",
